e7491540ff6442e97acd089febcb0e1858c58054
Htoo Thet Naung <htoothetnaung295@gmail.com>
2025-02-23 18:22:08 +0630

    Prepare for deployment

--- src/test/java/com/spaService/ServiceBookingSystem/ServiceBookingSystemApplicationTests.java
+++ src/test/java/com/spaService/ServiceBookingSystem/ServiceBookingSystemApplicationTests.java
@@ -3,7 +3,7 @@ package com.spaService.ServiceBookingSystem;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ServiceBookingSystemApplication.class)
 class ServiceBookingSystemApplicationTests {
 
 	@Test
